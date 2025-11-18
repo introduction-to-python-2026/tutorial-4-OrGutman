9f8521a0ed7e750b8013511186e6a919a75b46b6
OrGutman <or.gutman7@gmail.com>
2025-11-19 00:09:07 +0200

    Update orf.py

--- orf.py
+++ orf.py
@@ -5,39 +5,32 @@ def find_all_starts(seq):
             starts.append(i)
     return starts
 
-
 def find_first_in_register_stop(seq):
     stop_codons = {"TGA", "TAG", "TAA"}
-    n = len(seq)
-
-    for i in range(0, n - 2):
-        codon = seq[i:i+3]
-
-        if codon in stop_codons:
-            if (n - (i + 3)) % 3 == 0:
-                return i
-
+    start = seq.find("ATG")
+    if start == -1:
+        return -1
+    for i in range(start + 3, len(seq) - 2):
+        if seq[i:i+3] in stop_codons:
+            if (i - start) % 3 == 0:
+                return i + 3
     return -1
-
 
 def all_orfs_range(seq):
     orfs = []
     starts = find_all_starts(seq)
-
     for start in starts:
-        stop_index = find_first_in_register_stop(seq[start:])
-        
-        if stop_index != -1:
-            stop_index = start + stop_index
-            orfs.append((start, stop_index + 3))
-
+        for i in range(start + 3, len(seq) - 2):
+            codon = seq[i:i+3]
+            if codon in {"TGA", "TAG", "TAA"}:
+                if (i - start) % 3 == 0:
+                    orfs.append((start, i + 3))
+                    break
     return orfs
-
 
 def longest_orf(seq):
     orfs = all_orfs_range(seq)
     if not orfs:
         return ""
-    
     start, stop = max(orfs, key=lambda x: x[1] - x[0])
     return seq[start:stop]
