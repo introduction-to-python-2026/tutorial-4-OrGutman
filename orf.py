14b10e86f6a77d0d0852b15af3fc211c6939e6e9
OrGutman <or.gutman7@gmail.com>
2025-11-19 00:07:04 +0200

    Update orf.py

--- orf.py
+++ orf.py
@@ -8,23 +8,28 @@ def find_all_starts(seq):
 
 def find_first_in_register_stop(seq):
     stop_codons = {"TGA", "TAG", "TAA"}
-    for i in range(0, len(seq), 3):
-        if seq[i:i+3] in stop_codons:
-            return i
+    n = len(seq)
+
+    for i in range(0, n - 2):
+        codon = seq[i:i+3]
+
+        if codon in stop_codons:
+            if (n - (i + 3)) % 3 == 0:
+                return i
+
     return -1
 
 
 def all_orfs_range(seq):
-    stop_codons = {"TGA", "TAG", "TAA"}
-    starts = find_all_starts(seq)
     orfs = []
+    starts = find_all_starts(seq)
 
     for start in starts:
-        for i in range(start + 3, len(seq), 3):
-            codon = seq[i:i+3]
-            if codon in stop_codons:
-                orfs.append((start, i + 3))
-                break
+        stop_index = find_first_in_register_stop(seq[start:])
+        
+        if stop_index != -1:
+            stop_index = start + stop_index
+            orfs.append((start, stop_index + 3))
 
     return orfs
 
@@ -33,5 +38,6 @@ def longest_orf(seq):
     orfs = all_orfs_range(seq)
     if not orfs:
         return ""
+    
     start, stop = max(orfs, key=lambda x: x[1] - x[0])
     return seq[start:stop]
